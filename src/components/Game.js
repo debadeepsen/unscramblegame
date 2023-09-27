d11880fa8fc8ecf6aa5b3f2e5e8b19de91de4ad8
debadeepsensl <124399771+debadeepsensl@users.noreply.github.com>
2023-09-27 23:58:16 +0530

    game

--- src/components/Game.js
+++ src/components/Game.js
@@ -20,11 +20,11 @@ const Game = () => {
       <div className='flex'>
         <div></div>
         <div>
-            <h2>Rules</h2>
+            <h2 className='ml-5'>Rules</h2>
             <ul>
                 <li>All movies are from the IMDb top 50 list</li>
                 <li>If a movie has a space in its name, it will stay in its place, with the characters jumbled</li>
-                <li>Any apostrophe (&lsquot;) will be omitted</li>
+                <li>Any apostrophe (&apos;) will be omitted</li>
                 <li>In case of a movie being part of a series/franchise (<em>e.g.</em> Captain America: The First Avenger), only the second part will be considered</li>
             </ul>
         </div>
