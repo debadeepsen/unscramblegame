90efa8b57937191d53f91cfbd9e44204c7c777d6
debadeepsensl <124399771+debadeepsensl@users.noreply.github.com>
2023-09-28 11:39:21 +0530

    load movies

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,9 +10,12 @@ const Game = () => {
     setMovies(topMovies)
   }
 
+  const getCurrentMovie = () => {
+    return currentMovie
+  }
+
   useEffect(() => {
-    loadMovies()  
-    
+    loadMovies()
   }, [])
 
   return (
@@ -20,13 +23,29 @@ const Game = () => {
       <div className='flex'>
         <div></div>
         <div>
+          <div>
             <h2 className='ml-5'>Rules</h2>
             <ul>
-                <li>All movies are from the IMDb top 50 list</li>
-                <li>If a movie has a space in its name, it will stay in its place, with the characters jumbled</li>
-                <li>Any apostrophe (&apos;) will be omitted</li>
-                <li>In case of a movie being part of a series/franchise (<em>e.g.</em> Captain America: The First Avenger), only the second part will be considered</li>
+              <li>All movies are from the IMDb top 50 list</li>
+              <li>
+                If a movie has a space in its name, it will stay in its place,
+                with the characters jumbled
+              </li>
+              <li>Any apostrophe (&apos;) will be omitted</li>
+              <li>
+                In case of a movie being part of a series/franchise (
+                <em>e.g.</em> Captain America: The First Avenger), only the
+                second part will be considered
+              </li>
             </ul>
+            <button
+              onClick={() => {
+                document.write(getCurrentMovie())
+              }}
+            >
+              Begin Game
+            </button>
+          </div>
         </div>
       </div>
     </>
